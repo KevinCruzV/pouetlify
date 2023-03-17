2e4c231fd433a444ab54e9b175b5e248b9af6f3b
Stanley.js <jamais.stanley@gmail.com>
2023-03-17 14:03:33 +0100

    revert

--- symfony5/src/Kernel.php
+++ symfony5/src/Kernel.php
@@ -9,17 +9,3 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
-
-class AppKernel extends BaseKernel
-{
-    public function registerBundles()
-    {
-        $bundles = [
-            // ...
-
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-        ];
-
-        // ...
-    }
-}
