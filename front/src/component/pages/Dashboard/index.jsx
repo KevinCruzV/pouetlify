90e90f28788d8542d1124137f1944b4cbe72648e
KevinCruzV <72854509+KevinCruzV@users.noreply.github.com>
2023-03-17 16:23:39 +0100

    js

--- front/src/component/pages/Dashboard/index.jsx
+++ front/src/component/pages/Dashboard/index.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import DashBoard from '../../DashBoard'
 import styles from './style.module.css'
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 export default function DashBoardPage() {
+    const [selectedOption, setSelectedOption] = useState('');
+    const navigate = useNavigate();
 
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            option: selectedOption
+        };
+
+        axios.post('/api/generator', data)
+            .then(response => {
+                console.log(response);
+                navigate('/dashboard-page');
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
   
   return (
     <div className={styles.container}>
