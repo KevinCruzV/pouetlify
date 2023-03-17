e1ffc48763a6f9c57bde5a488d9731ac5f53796c
Stanley.js <jamais.stanley@gmail.com>
2023-03-17 14:01:58 +0100

    fix

--- symfony5/src/Kernel.php
+++ symfony5/src/Kernel.php
@@ -9,3 +9,16 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = [
+            // ...
+
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+        ];
+
+        // ...
+    }
